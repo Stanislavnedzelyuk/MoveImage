dd700487392efa0036e8e4d8197e26df68902c42
stanislav <stanislav.nedzelyuk@icloud.com>
2024-11-01 18:41:26 +0300

    Добавил обработку файлов с одинаковым названием файлов

--- moveimage.sh
+++ moveimage.sh
@@ -2,7 +2,7 @@
 
 # Проверка на передачу директории
 if [ -z "$1" ]; then
-    echo "Ошибка: Не указан путь к верхней папке. Использование: $0 </Users/stanislav/Downloads/1" >&2
+    echo "Ошибка: Не указан путь к верхней папке. Использование: $0 <путь к верхней папке>" >&2
     exit 1
 fi
 
@@ -19,9 +19,31 @@ fi
 LOG_FILE="move_images.log"
 echo "Логирование ошибок в файл: $LOG_FILE"
 
+# Функция для безопасного перемещения файла
+move_file() {
+    local file_path="$1"
+    local dest_dir="$2"
+    local filename=$(basename "$file_path")
+    local base_name="${filename%.*}"
+    local extension="${filename##*.}"
+    local counter=1
+
+    # Проверяем, существует ли файл с таким именем в целевой директории
+    while [ -e "$dest_dir/$filename" ]; do
+        # Добавляем номер к имени файла
+        filename="${base_name}_${counter}.${extension}"
+        counter=$((counter + 1))
+    done
+
+    # Перемещаем файл
+    mv "$file_path" "$dest_dir/$filename" 2>> "$LOG_FILE"
+}
+
 # Перемещение всех изображений из поддиректорий в верхнюю директорию
 echo "Начинается перемещение изображений из поддиректорий в: $TOP_DIR"
-find "$TOP_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -exec mv {} "$TOP_DIR" \; 2>> "$LOG_FILE"
+find "$TOP_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | while read -r file; do
+    move_file "$file" "$TOP_DIR"
+done
 
 # Проверка, были ли ошибки
 if [ $? -eq 0 ]; then
@@ -29,4 +51,3 @@ if [ $? -eq 0 ]; then
 else
     echo "Произошли ошибки при перемещении изображений. Подробности в файле $LOG_FILE" >&2
 fi
-
